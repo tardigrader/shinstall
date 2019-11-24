2621dd82312ffbbdede55b54cd3bf3ccfac230c3
Krister Löfgren <k@xyzzy.nu>
2019-11-24 13:48:41 +0100

    Initial setup of quicklisp

---
+++ setup_quicklisp.lisp
@@ -0,0 +1,6 @@
+(load "~/source/lisp/quicklisp/quicklisp.lisp")
+(quicklisp-quickstart:install)
+(ql:add-to-init-file)
+
+;; To make this non-interactive there should be
+;; a way to send a Enter keypress
